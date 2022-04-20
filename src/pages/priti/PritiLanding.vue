7b734a38f08152765be821d670443ee11d2c4738
athallahannafis <athallahannafis@gmail.com>
2022-04-20 08:29:31 +0700

    Implement salon card as component

--- src/pages/priti/PritiLanding.vue
+++ src/pages/priti/PritiLanding.vue
@@ -57,21 +57,13 @@
                 <h1 class="mb-5">Mitra Kami</h1>
                 <Carousel :value="salons" :numVisible="5" :numScroll="1" :circular="true" class="custom-carousel">
                     <template #item="salon">
-                        <div class="salon-container">
-                            <div class="salon-photo">
-                                <img :src="salon.data.image" :alt="salon.data.name" class="product-image" />
-                            </div>
-                            <div>
-                                <h4 class="mt-1">
-                                    {{ salon.data.name }}
-                                </h4>
-                                <h6 class="mt-0 mb-3">Kota {{ salon.data.city }}</h6>
-                                <div class="salon-footer mt-1">
-                                    <img class="star mr-2" :src="'images/priti/star.png'"/>
-                                    <p>{{ salon.data.rating }} | {{ salon.data.transactions }} transaksi</p>
-                                </div>
-                            </div>
-                        </div>
+                        <salon-card 
+                        :name="salon.data.name"
+                        :image="salon.data.image"
+                        :city="salon.data.city" 
+                        :rating="salon.data.rating"
+                        :transactions="salon.data.transactions"
+                        />
                     </template>
                 </Carousel>
             
@@ -83,14 +75,14 @@
 <script>
 import PritiTopBar from './PritiTopBar.vue';
 // import Paginator from 'primevue/paginator';
-// import SalonCard from './SalonCard.vue'
+import SalonCard from './SalonCard.vue'
 
 
 export default {
     components: {
         'PritiTopBar': PritiTopBar,
         // 'Paginator': Paginator,
-        // 'SalonCard': SalonCard,
+        'SalonCard': SalonCard,
     },
     data(){ 
         return {
@@ -354,40 +346,9 @@ export default {
     font-weight: 600;
     color:#005faa;
 }
-.salon {
-    margin-left: 2rem;
-    margin-right: 2rem;
-}
 .custom-carousel {
     min-width: 100rem; 
     max-width:100rem;
 }
-.salon-container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    background-color: white;
-    margin-left: 2px;
-    margin-right: 2px;
-    width: fit-content;
-    border-radius: 15px;
-    padding: 1.5em;
-    box-shadow: 0px 3px 5px rgba(0,0,0,.02), 0px 0px 2px rgba(0,0,0,.05), 0px 1px 4px rgba(0,0,0,.08);
 
-}
-.salon-container h4 {
-    font-family: 'Open Sans', sans-serif;
-    color: #747474;
-    font-weight: 600;
-}
-.salon-footer {
-    /* background: red; */
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-}
-.star {
-    width: 1em;
-    height: 1em;
-}
 </style>
---
+++ src/pages/priti/SalonCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="salon-container">
+        <div class="salon-photo">
+            <img :src="image" :alt="name" class="product-image" />
+        </div>
+        <div>
+            <h4 class="mt-1">
+                {{ name }}
+            </h4>
+            <h6 class="mt-0 mb-3">Kota {{ city }}</h6>
+            <div class="salon-footer mt-1">
+                <img class="star mr-2" :src="'images/priti/star.png'"/>
+                <p>{{ rating }} | {{ transactions }} transaksi</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        propTest: String,
+        name: String,
+        image: String,
+        rating: Number,
+        city: String,
+        transactions: Number,
+    }
+}
+</script>
+
+<style scoped>
+.salon-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    background-color: white;
+    margin-left: 2px;
+    margin-right: 2px;
+    width: fit-content;
+    border-radius: 15px;
+    padding: 1.5em;
+    box-shadow: 0px 3px 5px rgba(0,0,0,.02), 0px 0px 2px rgba(0,0,0,.05), 0px 1px 4px rgba(0,0,0,.08);
+
+}
+.salon-container h4 {
+    font-family: 'Open Sans', sans-serif;
+    color: #747474;
+    font-weight: 600;
+}
+.salon-footer {
+    /* background: red; */
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.star {
+    width: 1em;
+    height: 1em;
+}
+</style>
